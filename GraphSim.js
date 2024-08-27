92b776884461c1014568ea1887cc91527f0fb1c6
Dominik Pakuła <dominik@dmpk.xyz>
2024-08-27 13:27:27 +0200

    changed the code a little

--- GraphSim.js
+++ GraphSim.js
@@ -1,51 +1,72 @@
 class GraphSim {
-    #canvas
-    #timeouts = []
+    #canvas;
+    #ctx;
+    #timeouts = [];
+
+    #scaleX = 50;
+    #scaleY = 50;
+
+    get scaleX() {
+        return this.#scaleX;
+    }
+    set scaleX(scaleX) {
+        this.#scaleX = scaleX;
+    }
+
+    get scaleY() {
+        return this.#scaleY;
+    }
+    set scaleY(scaleY) {
+        this.#scaleY = scaleY;
+    }
 
     constructor(canvasId) {
         this.#canvas = document.getElementById(canvasId);
+        this.#ctx = this.#canvas.getContext('2d');
         this.render();
     }
 
-    render(func, scaleX, scaleY) {
+    render(func) {
         if (!this.#canvas.getContext)
             return;
 
-        const canvas = this.#canvas;
-        const ctx = canvas.getContext("2d");
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+        // clear currents
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
         this.#timeouts.forEach(clearTimeout)
         this.#timeouts = []
 
-        this.#drawXYAxis(canvas, ctx);
-        this.#drawRes(canvas, ctx, func, scaleX, scaleY);
-
+        this.#drawXYAxis();
+        this.#drawRes(func);
     }
 
-    #drawXYAxis(canvas, ctx) {
+    #drawXYAxis() {
+        const canvas = this.#canvas;
+        const ctx = this.#ctx;
+
         ctx.fillRect(0, canvas.height / 2, canvas.width, 1) // X Axis
         ctx.fillRect(canvas.width / 2, 0, 1, canvas.height) // Y Axis
     }
 
-    #drawRes(canvas, ctx, func, scaleX = 1, scaleY = 1) {
+    #drawRes(func) {
         if (!func)
             return;
 
+        const canvas = this.#canvas;
         let lastX, lastY = null;
 
         for (let i = 0; i <= canvas.width; i++) {
+            // calculate values
             const x = i - canvas.width / 2;
-            const y = scaleY * math.evaluate(func, { x: x / scaleX })
+            const y = this.#scaleY * math.evaluate(func, { x: x / this.#scaleX })
 
             if (lastX != null && lastY != null)
-                this.#timeouts.push(setTimeout(this.#drawLineBetween, 4 * i, ctx, { x: lastX, y: lastY }, { x: i, y: -y + canvas.height / 2 }))
+                this.#timeouts.push(setTimeout(this.#drawLineBetween, 4 * i, this.#ctx, { x: lastX, y: lastY }, { x: i, y: -y + canvas.height / 2 }))
 
             lastX = i; lastY = -y + canvas.height / 2;
         }
     }
 
+    // it has to get ctx as timeout can't access private fields
     #drawLineBetween(ctx, p1, p2) {
         ctx.beginPath();
         ctx.moveTo(p1.x, p1.y);
